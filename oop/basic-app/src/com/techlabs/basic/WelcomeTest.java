db5a62ce0bfa49b618442b192a6a5e7903512dfc
Tejashree Wagh <waghtejashree@gmail.com>
2020-01-25 18:12:29 +0530

    first prog

---
+++ oop/basic-app/src/com/techlabs/basic/WelcomeTest.java
@@ -0,0 +1,35 @@
+package com.techlabs.basic;
+
+public class WelcomeTest {
+	public static void main(String[] args) {
+	
+		System.out.print(1.3);
+		System.out.print(34);
+		printinfo(false);
+		printinfo(0);
+		printinfo(0.1f);
+		printinfo(1);
+		
+	
+	}
+
+	public static void printinfo(String s) {
+		System.out.println();
+	}
+
+	public static void printinfo(int i) {
+		System.out.println();
+	}
+
+	public static void printinfo(char c) {
+		System.out.println();
+	}
+
+	public static void printinfo(float f) {
+		System.out.println();
+	}
+
+	public static void printinfo(boolean b) {
+		System.out.println();
+	}
+}
